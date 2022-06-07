deed92f5909cc21abcaf89bec56379da64d3b55a
Ugo Arzur <ugo.arzur@gmail.com>
2022-06-07 22:49:42 +0200

    default distDir

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build",
   images: {
     loader: 'imgix',
     path: 'https://example.com/myaccount/',
